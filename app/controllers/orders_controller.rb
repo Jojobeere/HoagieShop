38d5fcf92055f45a621479178786dda4107579ea
Little Bird <31522351+hummingly@users.noreply.github.com>
2019-02-01 23:36:24 +0100

    Wanting to move to my PC

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,9 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+
+  def new
+    @order = Order.new
+    @order.hoagies.build
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer, foreign_key: 'customer_id'
   has_many :hoagies
+  accepts_nested_attributes_for :hoagies
 end
