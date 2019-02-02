527cfb1a05a7ebebe71f3372024c9470b561545c
LittleBird <31522351+hummingly@users.noreply.github.com>
2019-02-02 14:12:18 +0100

    Ordering 1 Hoagie with Base works now

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,38 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order = Order.new
-    @order.hoagies.build
+    if session[:customer_id].nil?
+      redirect_to login_path
+    else
+      @order = Order.new
+      @order.customer_id = session[:customer_id]
+      @order.hoagies.build
+    end
   end
+
+  def create
+    @order = Order.find_by(customer_id: session[:customer_id], status: nil)
+    if @order.nil?
+      @order = Order.new
+      @order.customer_id = session[:customer_id]
+    end
+    @order.status = 'Waiting'
+    if @order.update(order_params)
+      redirect_to @order
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @hoagies = Hoagie.where(order_id: @order.id)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :status, :price, :hoagies_attributes => [:id, :base_id, :count])
+  end
+
 end
