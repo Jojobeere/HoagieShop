e47dce261d79e0df702208f389241503d27ca187
hummingly <31522351+hummingly@users.noreply.github.com>
2019-02-03 22:22:02 +0100

    More secure
    No statistic page can be accessed by a customer.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,5 @@
 class StatisticsController < ApplicationController
-  http_basic_authenticate_with name: 'admin247', password: 'secret!', only: [:index]
+  http_basic_authenticate_with name: 'admin247', password: 'secret!'
   def create
     @statistic = Statistic.new(statistic_params)
     redirect_to @statistic
