15c6e31a70f9c6fe3d9079fc4b6914230ec31cf6
hummingly <31522351+hummingly@users.noreply.github.com>
2019-02-03 13:47:09 +0100

    Fix merge conflicts leftovers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,29 +3,12 @@ class CommentsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @comment = @review.comments.create(comment_params)
-<<<<<<< HEAD
-    if @comment.save
-      redirect_to review_path(@review)
-    else
-      render 'new'
-    end
+    redirect_to review_path(@review)
   end
-
-  # vorheriger Code zur Sicherheit noch drinnen gelassen
-  # def create
-  #   @comment = Comment.new(comment_params)
-  #   if @comment.save
-  #     redirect_to @comment
-  #   else
-  #     render 'new'
-  #   end
-  # end
 
   def show
     @comment = Comment.find(params[:id])
-=======
     redirect_to review_path(@review)
->>>>>>> a6bb9372d4367791a70ff92ed16efcc8ba0c8fe0
   end
 
   def destroy
@@ -33,11 +16,7 @@ class CommentsController < ApplicationController
     @comment = @review.comments.find(params[:id])
     @comment.destroy
     redirect_to review_path(@review)
-<<<<<<< HEAD
-end
-=======
   end
->>>>>>> a6bb9372d4367791a70ff92ed16efcc8ba0c8fe0
 
   private
   def comment_params
